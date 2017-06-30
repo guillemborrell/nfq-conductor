b663ce3428bfc8d837799562e267969b0c9aeb9a
Guillem Borrell <guillemborrell@gmail.com>
2017-06-30 15:09:36 +0200

    Preparing everything for the conductor.

--- setup.py
+++ setup.py
@@ -3,16 +3,17 @@
 from setuptools import setup
 
 setup(
-    name="nfq",
-    description="NFQ Solutions base package",
+    name="nfq-conductor",
+    description="NFQ Solutions process manager",
     version="0.1.2",
     author="NFQ Solutions",
     author_email="solutions@nfq.es",
     packages=[
         'nfq',
+        'nfq.conductor'
         ],
     zip_safe=False,
-    install_requires=[],
+    install_requires=[nfq-logwrapper, psutil],
     include_package_data=True,
     setup_requires=[],
     tests_require=[]
